cb18b7757df15d1b28f67c07a4a3b7f2c7eab9a2
Lai Hong Yu <joyjay606@gmail.com>
2024-09-10 19:53:35 +0800

    Fixture of Swift Tool Version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@
 //THIS IS MADE BY LAI HONG YU IN SSTINC//
 //THIS IS MADE BY LAI HONG YU IN SSTINC//
 ///////////////////////////////////////////////////////////////////////////////////////
-
+//swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package
 import PackageDescription
 
 let package = Package(
